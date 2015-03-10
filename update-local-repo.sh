d7cd6d547f2cbbc4756c6ee0ee6d2a869eff75a7
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-03-10 14:49:24 +0100

    Fix handling qubes-builder repo
    Use BUILDER_REPO_DIR variable where possible, fix repo initialization.

--- update-local-repo.sh
+++ update-local-repo.sh
@@ -4,11 +4,15 @@ set -e
 
 [ -z "$1" ] && { echo "Usage: $0 <dist>"; exit 1; }
 
-REPO_DIR=$PWD/qubes-packages-mirror-repo/$1
+REPO_DIR=$BUILDER_REPO_DIR
 DIST=$1
 
 mkdir -p $REPO_DIR/rpm
-createrepo --update -q $REPO_DIR
+if [ -e $REPO_DIR/repodata/repomd.xml ]; then
+    createrepo --update -q $REPO_DIR
+else
+    createrepo -q $REPO_DIR
+fi
 
 if [ `id -u` -eq 0 ]; then
     chown -R --reference=$REPO_DIR/.. $REPO_DIR
