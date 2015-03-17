5cd1c5f53b3f3012afc7b14df9f38ddf13a3c373
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-03-17 23:44:42 +0100

    Get rid of duplicated keys, also remove base_rpms which are downloaded

--- template_scripts/00_prepare.sh
+++ template_scripts/00_prepare.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-pushd $SCRIPTSDIR
-rm -f keys base_rpms
-ln -sf keys_$DIST keys
-ln -sf base_rpms_$DIST base_rpms
-popd
-
 createrepo -q -g $SCRIPTSDIR/../comps-qubes-template.xml pkgs-for-template/$DIST -o pkgs-for-template/$DIST
 chown -R --reference=pkgs-for-template/$DIST pkgs-for-template/$DIST
