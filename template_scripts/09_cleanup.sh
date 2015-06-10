43a80850072d402ae85c46458bd611c0337e2ae8
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-06-10 11:32:31 +0200

    template: remove random-seed
    Prevent all the templates using the same initial random seed.

--- template_scripts/09_cleanup.sh
+++ template_scripts/09_cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 rm -f $INSTALLDIR/var/lib/rpm/__db.00* $INSTALLDIR/var/lib/rpm/.rpm.lock
+rm -f $INSTALLDIR/var/lib/systemd/random-seed
 yum -c $SCRIPTSDIR/../template-yum.conf $YUM_OPTS clean packages --installroot=$INSTALLDIR
 
 # Make sure that rpm database has right format (for rpm version in template, not host)
