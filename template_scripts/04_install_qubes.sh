9a0fc04c5574bde6e2410e8793bbe47410fdba2e
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2017-06-08 01:16:07 +0200

    template: move yum.q-o.o repo setup into appropriate script
    Install packages from yum.qubes-os.org repo in 04_install_qubes.sh, not
    necessary any group install
    
    QubesOS/qubes-issues#2572

--- template_scripts/04_install_qubes.sh
+++ template_scripts/04_install_qubes.sh
@@ -6,6 +6,22 @@ prepareChroot
 
 export YUM0=$PWD/pkgs-for-template
 
+cp ${SCRIPTSDIR}/template-builder-repo.repo ${INSTALLDIR}/etc/yum.repos.d/
+if [ -n "$USE_QUBES_REPO_VERSION" ]; then
+    sed -e "s/%QUBESVER%/$USE_QUBES_REPO_VERSION/g" \
+        < ${SCRIPTSDIR}/../repos/qubes-repo-vm.repo \
+        > ${INSTALLDIR}/etc/yum.repos.d/template-qubes-vm.repo
+    keypath="${BUILDER_DIR}/qubes-release-${USE_QUBES_REPO_VERSION}-signing-key.asc"
+    if [ -r "$keypath" ]; then
+        # use stdin to not copy the file into chroot. /dev/stdin
+        # symlink doesn't exists there yet
+        chroot_cmd rpm --import /proc/self/fd/0 < "$keypath"
+    fi
+    if [ "0$USE_QUBES_REPO_TESTING" -gt 0 ]; then
+        yumConfigRepository enable 'qubes-builder-*-current-testing'
+    fi
+fi
+
 echo "--> Installing RPMs..."
 if [ "$TEMPLATE_FLAVOR" != "minimal" ]; then
     installPackages packages_qubes.list || RETCODE=1
@@ -37,5 +53,8 @@ fi
 # Distribution specific steps
 source ./functions.sh
 buildStep "${0}" "${DIST}"
+
+rm -f ${INSTALLDIR}/etc/yum.repos.d/template-builder-repo.repo
+rm -f ${INSTALLDIR}/etc/yum.repos.d/template-qubes-vm.repo
 
 exit $RETCODE
