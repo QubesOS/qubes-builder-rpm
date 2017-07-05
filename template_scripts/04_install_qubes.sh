ba35cd6cf61a204fb3d47458e32eb852df930eb2
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2017-07-05 12:34:45 +0200

    template: do not mount modules.img when booting VM-provided kernel
    Mounting /lib/modules/$(uname -r) from dom0 is not needed when the
    kernel is provided by the VM itself. In fact this script would do
    nothing in such a case anyway.
    
    QubesOS/qubes-issues#2577

--- template_scripts/04_install_qubes.sh
+++ template_scripts/04_install_qubes.sh
@@ -64,7 +64,7 @@ if [ "0$TEMPLATE_ROOT_WITH_PARTITIONS" -eq 1 ]; then
     dev=${dev%p?}
     for kver in $(ls ${INSTALLDIR}/lib/modules); do
         chroot_cmd dkms autoinstall -k "$kver" || RETCODE=1
-        chroot_cmd dracut -f -a "qubes-vm qubes-vm-modules" \
+        chroot_cmd dracut -f -a "qubes-vm" \
             /boot/initramfs-${kver}.img ${kver} || RETCODE=1
     done
     chroot_cmd grub2-install "$dev" || RETCODE=1
