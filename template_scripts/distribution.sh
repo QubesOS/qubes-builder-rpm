7341dadd6b86a8ef9d2ac0e4f84524360ee72ab0
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2018-05-01 01:56:19 +0200

    Even less bashism

--- template_scripts/distribution.sh
+++ template_scripts/distribution.sh
@@ -87,14 +87,14 @@ function yumInstall() {
             install ${YUM_OPTS} -y ${files[@]} || exit 1
         find ${INSTALLDIR}/var/cache/dnf -name '*.rpm' -print0 | xargs -r0 sha256sum
         find ${INSTALLDIR}/var/cache/yum -name '*.rpm' -print0 | xargs -r0 sha256sum
-        if [ "$DISTRIBUTION" == "fedora" ]; then
+        if [ "$DISTRIBUTION" = "fedora" ]; then
             # set http proxy to invalid one, to prevent any connection in case of
             # --cacheonly being buggy: better fail the build than install something
             # else than the logged one
             chroot_cmd $YUM install ${YUM_OPTS} -y \
                 --cacheonly --setopt=proxy=http://127.0.0.1:1/ ${files[@]} || exit 1
         fi
-        if [ "$DISTRIBUTION" == "centos" ]; then
+        if [ "$DISTRIBUTION" = "centos" ]; then
             # Temporarly disable previous strategy (problem with downloading cache qubes template repo)
             chroot_cmd $YUM install ${YUM_OPTS} -y ${files[@]} || exit 1
         fi
@@ -131,14 +131,14 @@ function yumGroupInstall() {
             group install $optional ${YUM_OPTS} -y ${files[@]} || exit 1
         find ${INSTALLDIR}/var/cache/dnf -name '*.rpm' -print0 | xargs -r0 sha256sum
         find ${INSTALLDIR}/var/cache/yum -name '*.rpm' -print0 | xargs -r0 sha256sum
-        if [ "$DISTRIBUTION" == "fedora" ]; then
+        if [ "$DISTRIBUTION" = "fedora" ]; then
             # set http proxy to invalid one, to prevent any connection in case of
             # --cacheonly being buggy: better fail the build than install something
             # else than the logged one
             chroot_cmd $YUM install ${YUM_OPTS} -y \
                 --cacheonly --setopt=proxy=http://127.0.0.1:1/ ${files[@]} || exit 1
         fi
-        if [ "$DISTRIBUTION" == "centos" ]; then
+        if [ "$DISTRIBUTION" = "centos" ]; then
             # Temporarly disable previous strategy (problem with downloading cache qubes template repo)
             chroot_cmd $YUM install ${YUM_OPTS} -y ${files[@]} || exit 1
         fi
