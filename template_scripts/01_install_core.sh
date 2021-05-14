ee74a38c505e4ea5c11360fd94d7d07e2bd1f165
Demi Marie Obenour <demi@invisiblethingslab.com>
2021-05-14 17:36:54 -0400

    Always build the RPMDB from scratch
    Fedora 34 does not support the old Berkeley DB RPMDB, so we need to
    rebuild the RPMDB from scratch.
    
    Suggested-by: Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>

--- template_scripts/01_install_core.sh
+++ template_scripts/01_install_core.sh
@@ -6,14 +6,12 @@ source "${SCRIPTSDIR}/distribution.sh"
 export YUM_OPTS
 ${SCRIPTSDIR}/../prepare-chroot-base "${INSTALLDIR}" "${DIST}"
 
-if grep -q openSUSE /etc/os-release; then
-    # Build the rpmdb again, in case of huge rpm version difference that makes
-    # rpmdb --rebuilddb doesn't work anymore. Export using rpm from outside
-    # chroot and import using rpm from within chroot
-    rpmdb "${RPM_OPTS[@]}" --root="${INSTALLDIR}" --exportdb > "${CACHEDIR}/rpmdb.export" || exit 1
-    rm -rf "${INSTALLDIR}/var/lib/rpm"
-    chroot "${INSTALLDIR}" rpmdb --importdb < "${CACHEDIR}/rpmdb.export" || exit 1
-fi
+# Build the rpmdb again, in case of huge rpm version difference that makes
+# rpmdb --rebuilddb doesn't work anymore. Export using rpm from outside
+# chroot and import using rpm from within chroot
+rpmdb "${RPM_OPTS[@]}" --root="${INSTALLDIR}" --exportdb > "${CACHEDIR}/rpmdb.export" || exit 1
+rm -rf "${INSTALLDIR}/var/lib/rpm"
+chroot "${INSTALLDIR}" rpmdb --importdb < "${CACHEDIR}/rpmdb.export" || exit 1
 
 # remove systemd-resolved symlink
 rm -f "${INSTALLDIR}/etc/resolv.conf"
