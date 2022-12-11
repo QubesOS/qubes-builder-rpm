3b3bfa166df25aa1f90cb5e8c38aefa7ac089ad4
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2022-12-11 20:03:20 +0100

    Fix SPDX entries

--- template_rpm/02_install_groups.sh
+++ template_rpm/02_install_groups.sh
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License along
 # with this program. If not, see <https://www.gnu.org/licenses/>.
 #
-# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-License-Identifier: GPL-2.0-or-later
 
 # shellcheck source=qubesbuilder/plugins/template_rpm/distribution.sh
 source "${TEMPLATE_CONTENT_DIR}/distribution.sh"
