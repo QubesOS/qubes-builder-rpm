e918586d4bda4ba4499e0ac3a2ba72fe4f0ae12c
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2022-12-19 09:39:08 +0100

    04_install_qubes.sh: use INSTALL_DIR for fuser

--- template_rpm/04_install_qubes.sh
+++ template_rpm/04_install_qubes.sh
@@ -119,11 +119,7 @@ if ! containsFlavor "minimal" && [ "0$TEMPLATE_ROOT_WITH_PARTITIONS" -eq 1 ]; th
     done
     chroot_cmd grub2-install --target=i386-pc "$dev" || RETCODE=1
     chroot_cmd grub2-mkconfig -o /boot/grub2/grub.cfg || RETCODE=1
-    if [ "0${IS_LEGACY_BUILDER}" -eq 1 ]; then
-        fuser -kMm "${INSTALL_DIR}"
-    else
-        fuser -kMm @BUILDER_DIR@/mnt
-    fi
+    fuser -kMm "${INSTALL_DIR}"
     chroot_cmd umount /sys /dev
 fi
 
