QubesOS/qubes-builder-rpm

commit ec78f6dc7759feeb02c14bf3d461b43a879c5e08

Fix missing quotes
Issue detected by shellcheck. In practice $RETCODE is always set to a
single word, but lets make shellcheck happy anyway.

template_rpm/04_install_qubes.sh
@@ -136,4 +136,4 @@ buildStep "${0}" "${DIST_CODENAME}"
 rm -f "${INSTALL_DIR}/etc/yum.repos.d/template-builder-repo-${DIST_NAME}.repo"
 rm -f "${INSTALL_DIR}/etc/yum.repos.d/template-qubes-vm.repo"
 
-exit $RETCODE
+exit "$RETCODE"

commit 7077740932671a2c3dc1eecdcaf84780ec9a1443

Initialize RETCODE variable
exit doesn't take empty string

Fixes: ec78f6d "Fix missing quotes"

template_rpm/04_install_qubes.sh
@@ -23,6 +23,8 @@
 # shellcheck source=template_rpm/distribution.sh
 source "${TEMPLATE_CONTENT_DIR}/distribution.sh"
 
+RETCODE=0
+
 # Prepare system mount points
 prepareChroot
 